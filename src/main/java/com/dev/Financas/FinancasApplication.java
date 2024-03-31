2f4ee29c877c64d1066fc2208497e8f61e700b2c
Tiago Marques de Oliveira <lezzadogamer@gmail.com>
2024-03-31 18:21:41 -0300

    Update FinancasApplication.java

--- src/main/java/com/dev/Financas/FinancasApplication.java
+++ src/main/java/com/dev/Financas/FinancasApplication.java
@@ -2,11 +2,20 @@ package com.dev.Financas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FinancasApplication {
-
+@EnableWebMvc
+public class FinancasApplication implements WebMvcConfigurer{
 	
+	@Override
+	public void addCorsMappings( CorsRegistry registry ) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
 	}
